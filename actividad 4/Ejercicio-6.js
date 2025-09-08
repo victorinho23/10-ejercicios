c874e7e85af7f013b87ee6b84527b638cb96d04a
Kevin <loaizakevin75@gmail.com>
2025-09-08 07:02:59 -0500

    Se hicieron los ejercicios 5 y 6

---
+++ actividad 4/Ejercicio-6.js
@@ -0,0 +1,9 @@
+// Solicita un número entero al usuario
+let numero = parseInt(promt("Ingrese un número entero:"));
+
+// Verifica si el número es par o impar
+if (numero % 2 === 0){
+    alert("El numero es par.");
+} else{
+    alert("El numero es impar.");
+}
